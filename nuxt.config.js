9db26dd0a501ac3593e053d6abe1f2faf4d71736
serii <seriiburduja@gmail.com>
2022-09-26 16:59:17 +0300

    feat: leaflet

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,54 +1,55 @@
-import { resolve } from "path";
+import {resolve} from "path";
 
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: "nuxt-segalerba",
-    htmlAttrs: {
-      lang: "en",
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "nuxt-segalerba",
+        htmlAttrs: {
+            lang: "en",
+        },
+        meta: [
+            {charset: "utf-8"},
+            {name: "viewport", content: "width=device-width, initial-scale=1"},
+            {hid: "description", name: "description", content: ""},
+            {name: "format-detection", content: "telephone=no"},
+        ],
+        link: [{rel: "icon", type: "image/x-icon", href: "/favicon.ico"}],
     },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+    router: {
+        linkActiveClass: "active",
+        linkExactActiveClass: "exact-active",
+    },
+    env: {
+        siteUrl: process.env.SITE_URL
+    },
+    alias: {
+        "@root": resolve(__dirname, "./"),
+    },
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ["~/assets/scss/my.scss"],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [{src: '~plugins/leaflet.js', ssr: false}],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        "@nuxtjs/axios",
+        "nuxt-leaflet",
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
-  router: {
-    linkActiveClass: "active",
-    linkExactActiveClass: "exact-active",
-  },
-  env: {
-    siteUrl: process.env.SITE_URL
-  },
-  alias: {
-    "@root": resolve(__dirname, "./"),
-  },
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/scss/my.scss"],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: process.env.API_URL,
+    },
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.API_URL,
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
 };

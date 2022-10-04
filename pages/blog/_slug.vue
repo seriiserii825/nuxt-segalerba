2332a73f7f3ddc469753c0c47050e47180c6cb8c
serii <seriiburduja@gmail.com>
2022-10-04 13:48:59 +0300

    feat: single-blog

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,136 +1,95 @@
 <template>
   <div class="single-article">
-    <div class="page-intro ">
-      <img :src="`${site_url}${blog.page_intro.image.data.attributes.url}`" alt=""/>
+    <div class="page-intro">
+      <img
+        :src="`${site_url}${blog.page_intro.image.data.attributes.url}`"
+        alt=""
+      />
       <div class="container">
-        <h1 class="page-intro__title">Mercato immobiliare: la sfida del recupero dell’entroterra.</h1>
+        <h1 class="page-intro__title" v-if="post.title">{{ post.title }}</h1>
       </div>
     </div>
     <div class="container">
       <div class="site-bg site-bg--padding">
         <ul class="breadcrumbs">
           <li>
-            <a href="https://immobiliaresegalerba.it/blog/">Blog</a>
+            <nuxt-link to="/blog">Blog</nuxt-link>
           </li>
-          <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 0.5L5 4L1 8" stroke="#555555" stroke-linecap="round"></path>
-            <path d="M4 0.5L8 4L4 8" stroke="#555555" stroke-linecap="round"></path>
+          <svg
+            width="9"
+            height="9"
+            viewBox="0 0 9 9"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 0.5L5 4L1 8"
+              stroke="#555555"
+              stroke-linecap="round"
+            ></path>
+            <path
+              d="M4 0.5L8 4L4 8"
+              stroke="#555555"
+              stroke-linecap="round"
+            ></path>
           </svg>
-          <li>
-            <a href="https://immobiliaresegalerba.it/category/mercato-immobiliare">mercato immobiliare</a>
-          </li>
-          <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 0.5L5 4L1 8" stroke="#555555" stroke-linecap="round"></path>
-            <path d="M4 0.5L8 4L4 8" stroke="#555555" stroke-linecap="round"></path>
-          </svg>
-          <li>
-            <span>Mercato immobiliare: la sfida del recupero dell’entroterra.</span>
+          <li v-if="post.title">
+            <span>{{ post.title }}</span>
           </li>
         </ul>
-        <div class="single-article__img">
-          <img width="1200" height="628"
-               src="https://immobiliaresegalerba.it/wp-content/uploads/2022/08/blog-glamping.png"
-               class="attachment-full size-full wp-post-image" alt="mercato immobiliare"
-               srcset="https://immobiliaresegalerba.it/wp-content/uploads/2022/08/blog-glamping.png 1200w, https://immobiliaresegalerba.it/wp-content/uploads/2022/08/blog-glamping-768x402.png 768w, https://immobiliaresegalerba.it/wp-content/uploads/2022/08/blog-glamping-400x209.png 400w"
-               sizes="(max-width: 1200px) 100vw, 1200px"></div>
+        <div
+          class="single-article__img"
+          v-if="post.image && post.image.data.attributes.url"
+        >
+          <img
+            width="1200"
+            height="628"
+            :src="`${site_url}${post.image.data.attributes.url}`"
+            class="attachment-full size-full wp-post-image"
+            alt="mercato immobiliare"
+          />
+        </div>
         <div class="single-article__content">
-          <p>La pandemia ha cambiato le nostre vite ed ha alterato la nostra percezione nell’abitare le città
-            metropolitane. Così, durante il primo lockdown nel 2020, con la fuga dalle città per raggiungere le seconde
-            case in montagna, al lago, al mare, gli italiani hanno riscoperto quelle aree marginali meglio note come
-            “aree interne o entroterra”. Complice lo smart working, anche quei territori un tempo dimenticati e snobbati
-            hanno assunto una nuova dimensione del mercato immobiliare.</p>
-          <p>Negli ultimi due anni abbiamo assistito alla nascita dal nulla di onniscienti discepoli che <strong>profetizzavano
-            una nuova vita </strong><strong>per le aree rurali</strong>, solamente per un elementare, quanto ingenuo,
-            processo di sottrazione: non posso più occupare le città a causa della pandemia, i comuni costieri della
-            Liguria costano troppo, allora vado a occupare altri territori da tempo disabitati, con le mie idee smart,
-            senza conoscere quali fattori, per lo più negativi e di sistema, le caratterizzano. Ed ecco sorgere
-            dibattiti totalmente privi di basi sociologiche e culturali che denotano soltanto scarsa conoscenza dei
-            territori, della loro storia e delle radici culturali che li caratterizzano.</p>
-          <p>Se leggiamo i dati delle aree interne, si delinea un quadro che ci fa comprendere immediatamente <strong>la
-            realtà delle cose</strong>: in Italia 10 milioni di abitanti vivono in aree interne carenti di servizi e
-            infrastrutture; 16 milioni di abitanti vivono in province molto deficitarie e prive di servizi pubblici di
-            ogni genere.</p>
-          <p>Purtroppo negli anni si sono susseguite politiche sbagliate che hanno favorito l’abbandono delle aree
-            alpine e appenniniche, senza ricambio generazionale, senza risolvere i problemi legati alle infrastrutture
-            di base (strade, ospedali di primo soccorso, scuole).</p>
-          <h2>Mercato immobiliare dell’entroterra ligure</h2>
-          <p>Dalla Nota Territoriale di Statistica del compravenduto in Liguria, elaborata dalla Direzione Regionale
-            dell’Agenzia delle Entrate e dai Dipartimenti Provinciali del Territorio, è emerso che nell’area della Città
-            Metropolitana di Genova nel corso del 2021 sono state effettuate <strong>1295 compravendite nelle cosiddette
-              “aree dell’entroterra”</strong>, rappresentando l’1,79% dell’IMI (differenza % tra il compravenduto e lo
-            stock immobiliare), ed analizzando in maniera analitica i dati si ha evidenza di quanto in appresso:</p>
-          <ul>
-            <li>Val Petronio 106 NTN (numero di transazioni normalizzato)</li>
-            <li>Valle Stura 122 NTN</li>
-            <li>Valle Scrivia 693 NTN</li>
-            <li>Val Fontanabuona 374 NTN</li>
-          </ul>
-          <p>In Italia vi sono stati diversi casi dove architettura e politica hanno determinato il
-            <strong>recupero </strong><strong>edilizio e sociale</strong> di una serie di paesini dimenticati,
-            attraverso progetti mirati a rivitalizzare borghi e frazioni dimenticate. Grazie al lavoro sapiente dei
-            progettisti, si è manifestata la rivalorizzazione di singole porzioni di paesi, differenziandone le
-            funzioni, riattivando sia la condizione demografica sia un’economia di sussistenza, consentendo così a
-            luoghi abbandonati di ritornare alla vita.</p>
-          <h2>La Borgata Alpisella di Garessio ed il progetto Selucente</h2>
-          <p>Se le aree interne non sono attrattive per i progettisti, esistono comunque realtà interessanti come è
-            accaduto recentemente con il recupero della borgata Alpisella di Garessio, in provincia di Cuneo, sulle Alpi
-            Liguri al confine tra le province di Savona ed Imperia, con il <a href="http://www.selucente.it"
-                                                                              target="_blank" rel="noopener">progetto
-              Selucente</a>.<br>
-            Curato dallo Studio di Architettura Officina82 degli architetti Fabio Revetria e Lara Sappa, gratificati
-            anche con il prestigioso premio di architettura internazionale “BIGSEE – Tourism Design Award Winter 2022” a
-            Lubiana.</p>
-          <p>In quella che era la piccola chiesa della piccola località è stata ricavata l’accoglienza dei turisti,
-            mentre nella restante parte dell’area sono state allestite la Glam Box e la StarBox.</p>
-          <p>La <strong>GlamBox</strong> è un modulo ricettivo in legno ispirato all’architettura rurale delle Alpi
-            Liguri. Le sue aperture, così come i tagli nella copertura, offrono una vista privilegiata sul paesaggio
-            circostante. La particolarità di GlamBOX è il letto, pensato su ruote per essere comodamente portato sulla
-            pedana antistante e ammirare il cielo stellato.</p>
-          <p>La <strong>Starbox</strong> è invece l’esperienza glamping più innovativa del momento, una casetta in legno
-            col tetto apribile per stare sdraiati sotto il cielo stellato.<br>
-            Un progetto innovativo che dimostra quanto sia fattibile per i piccoli borghi italiani una possibilità di
-            rinascita sociale ed economica, dovuta in particolare al forte sviluppo della richiesta turistica.</p>
-          <p>L’Italia è costellata di tanti piccoli borghi storici medievali, ma anche da tanti piccoli comuni, entro i
-            5000 abitanti. Questi, sebbene privi di interesse storico e artistico, costituiscono quel nucleo importante
-            per la vita sociale e produttiva del nostro paese, spesso afflitti da un’economia spenta e da un mercato
-            immobiliare da tempo in crisi.</p>
-          <p><strong>Basterebbero davvero pochi, ma significativi progetti di riqualificazione</strong>, come quello
-            raccontato, per garantire una ripresa economica di quei territori, evitare lo spopolamento e far ripartire
-            il mercato immobiliare turistico. Occorre fare sistema, ma tutti assieme, le Istituzioni, gli amministratori
-            locali e tutti gli stakeholder che compongono il mercato immobiliare.</p>
-          <p>&nbsp;</p>
-          <p><a href="https://immobiliaresegalerba.it/chi-siamo/"><em><strong>Fabrizio Segalerba</strong></em></a></p>
-          <p><em>Agente immobiliare certificato – Segretario nazionale FIAIP</em></p>
-
-          <ul class="single-article__tags">
+          <div v-if="post.text" v-html="post.text"></div>
+          <ul
+            class="single-article__tags"
+            v-if="post.post_tags && post.post_tags.data.length > 0"
+          >
             <li><span>TAGS:</span></li>
-            <li><a href="https://immobiliaresegalerba.it/tag/entroterra/" title="entroterra Tag" class="entroterra">entroterra</a>
+            <li v-for="tag in post.post_tags.data" :key="tag.id">
+              <nuxt-link
+                :to="`/blog/tag/${tag.attributes.slug}`"
+                :title="tag.attributes.title"
+                class="mercato-immobiliare"
+              >
+                {{ tag.attributes.title }}
+              </nuxt-link>
             </li>
-            <li><a href="https://immobiliaresegalerba.it/tag/mercato-immobiliare/" title="mercato immobiliare Tag"
-                   class="mercato-immobiliare">mercato immobiliare</a></li>
           </ul>
         </div>
-
         <div class="single-author">
           <div class="single-author__img">
-            <img src="https://immobiliaresegalerba.it/wp-content/uploads/2021/06/fabrizio-segalerba.png" alt="">
+            <img
+              v-if="post.author && post.author.data.attributes.image"
+              :src="`${site_url}${post.author.data.attributes.image.data.attributes.url}`"
+              alt=""
+            />
           </div>
-          <div class="single-author__text">Fabrizio Segalerba è agente immobiliare nella sua Genova, titolare
-            dell'omonima agenzia dal 1994. Dedica molto tempo alla FIAIP (Federazione Italiana Agenti Immobiliari
-            Professionali) all'interno della quale ricopre il ruolo di Segretario Nazionale. Fabrizio è un
-            perfezionista, ama l'arte e la storia, è autore in questo blog perché crede che la condivisione di
-            esperienze e di informazioni possa migliorare il rapporto tra cliente e agente immobiliare.
+          <div
+            class="single-author__text"
+            v-if="post.author && post.author.data.attributes.text"
+          >
+            {{ post.author.data.attributes.text }}
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Blog',
-  async asyncData({store, params}) {
+  name: "Blog",
+  async asyncData({ store, params }) {
     let blog = null;
 
     if (!store.state.blog.blog) {
@@ -139,20 +98,20 @@ export default {
     }
     blog = store.state.blog.blog;
 
-    await store.dispatch("posts/fetchPost", {slug: params.slug});
-    const post = store.state.posts.post;
+    await store.dispatch("posts/fetchPost", { slug: params.slug });
+    const post = store.state.posts.post.data[0].attributes;
 
     return {
       blog: blog.data.attributes,
-      post: post
+      post: post,
     };
   },
   data() {
     return {
       site_url: process.env.siteUrl,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 <style lang="scss">
 </style>
--- store/posts.js
+++ store/posts.js
@@ -1,59 +1,64 @@
 export const state = () => ({
-    posts: [],
-    post: {}
+  posts: [],
+  post: {},
 });
 
 export const mutations = {
-    getLastPosts(state, payload) {
-        state.posts = payload;
-    },
-    getPost(state, payload) {
-        state.post = payload;
-    }
+  getLastPosts(state, payload) {
+    state.posts = payload;
+  },
+  getPost(state, payload) {
+    state.post = payload;
+  },
 };
 
 export const actions = {
-    async fetchPosts({commit}) {
-        const qs = require("qs");
-        const query = qs.stringify(
-            {
-                populate: {
-                    post_tags: {
-                        fields: ["title", "slug"],
-                    },
-                    image: {
-                        fields: ["url"],
-                    },
-                },
-            },
-            {
-                encodeValuesOnly: true, // prettify URL
-            }
-        );
-        const {data} = await this.$axios.get("/posts?" + query);
-        commit("getLastPosts", data);
-    },
-    async fetchPost({commit}, payload) {
-        const qs = require("qs");
-        const query = qs.stringify(
-            {
-                filter: {
-                  slug: payload.slug
-                },
-                populate: {
-                    post_tags: {
-                        fields: ["title", "slug"],
-                    },
-                    image: {
-                        fields: ["url"],
-                    },
-                },
-            },
-            {
-                encodeValuesOnly: true, // prettify URL
-            }
-        );
-        const {data} = await this.$axios.get("/posts/" + query);
-        commit("getPost", data);
-    },
+  async fetchPosts({ commit }) {
+    const qs = require("qs");
+    const query = qs.stringify(
+      {
+        populate: {
+          post_tags: {
+            fields: ["title", "slug"],
+          },
+          image: {
+            fields: ["url"],
+          },
+        },
+      },
+      {
+        encodeValuesOnly: true, // prettify URL
+      }
+    );
+    const { data } = await this.$axios.get("/posts?" + query);
+    commit("getLastPosts", data);
+  },
+  async fetchPost({ commit }, payload) {
+    const qs = require("qs");
+    const query = qs.stringify(
+      {
+        filters: {
+          slug: {
+            $eq: payload.slug,
+          },
+        },
+        populate: {
+          author: {
+            populate: "*",
+          },
+          post_tags: {
+            fields: ["title", "slug"],
+          },
+          image: {
+            fields: ["url"],
+          },
+        },
+      },
+      {
+        encodeValuesOnly: true, // prettify URL
+      }
+    );
+    const { data } = await this.$axios.get("/posts?" + query);
+    commit("getPost", data);
+  },
 };

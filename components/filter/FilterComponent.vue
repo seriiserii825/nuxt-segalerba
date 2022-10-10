815e7fc5ae599bfa57156f36fc1bccf6a6adf7f2
serii <seriiburduja@gmail.com>
2022-10-10 10:31:18 +0300

    feat: provincia works

--- components/filter/FilterComponent.vue
+++ components/filter/FilterComponent.vue
@@ -5,7 +5,50 @@
       <div class="filter__label">Trova il tuo immobile</div>
       <div class="filter home-intro__filter">
         <div class="filter-body">
-          <div class="filter-block" id="residentiale"></div>
+          <div class="filter-block" id="residentiale">
+            <div
+              class="filter__item"
+              v-if="provincia_immobile && provincia_immobile.length > 0"
+            >
+              <select-component
+                tab-index="1"
+                :options="provincia_immobile"
+                id="id"
+                title="Provincia"
+                label="title"
+                :value="province"
+                @handle-func="setProvince"
+                loop-id="id"
+                svg=""
+              ></select-component>
+            </div>
+            <div class="filter__item">
+              <select-component
+                tab-index="1"
+                :options="localita_immobile"
+                id="id"
+                title="Località"
+                label="title"
+                :value="localita"
+                @handle-func="setLocalita"
+                loop-id="id"
+                svg=""
+              ></select-component>
+            </div>
+            <div class="filter__item">
+              <select-component
+                tab-index="1"
+                :options="zona_immobile"
+                id="id"
+                title="Zona"
+                label="title"
+                :value="zona"
+                @handle-func="setZona"
+                loop-id="id"
+                svg=""
+              ></select-component>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -14,6 +57,7 @@
 </template>
 <script>
 import Preloader from "~/components/filter/Preloader";
+import SelectComponent from "~/components/filter/SelectComponent";
 export default {
   name: "FilterComponent",
   data() {
@@ -21,20 +65,87 @@ export default {
       loading: false,
       site_url: process.env.siteUrl,
       provincia_immobile: null,
+      default_provincia_immobile: null,
+      localita_immobile: null,
+      default_localita_immobile: null,
+      zona_immobile: null,
+      province: "Tutte",
+      localita: "Tutte",
+      zona: "Tutte",
     };
   },
   components: {
     Preloader,
+    SelectComponent,
   },
-  async created() {
-    await this.$store.dispatch("filters/fetchProvinciaImmobile");
-    this.provincia_immobile = this.$store.state.filters.provincia_immobile;
-
-    await this.$store.dispatch("filters/fetchLocalitaImmobile");
-    this.provincia_immobile = this.$store.state.filters.localita_immobile;
-
-    await this.$store.dispatch("filters/fetchZonaImmobile");
-    this.provincia_immobile = this.$store.state.filters.zona_immobile;
+  methods: {
+    setProvince(value) {
+      this.province = value.label;
+    },
+    setLocalita(value) {
+      this.localita = value.label;
+    },
+    setZona(value) {
+      this.zona = value.label;
+    },
+    async setProvinceOptions() {
+      await this.$store.dispatch("filters/fetchProvinciaImmobile");
+      let provincia_immobile = this.$store.state.filters.provincia_immobile;
+      provincia_immobile = provincia_immobile.data.map((item) => {
+        return {
+          id: item.id,
+          title: item.attributes.title,
+          children: item.attributes.localita_immobiles.data,
+        };
+      });
+      this.provincia_immobile = [
+        {
+          id: "all",
+          title: "Tutte",
+          children: null,
+        },
+        ...provincia_immobile,
+      ];
+    },
+    async setLocalitaOptions() {
+      await this.$store.dispatch("filters/fetchLocalitaImmobile");
+      this.localita_immobile = this.$store.state.filters.localita_immobile;
+      this.localita_immobile = this.localita_immobile.data.map((item) => {
+        return {
+          id: item.id,
+          title: item.attributes.title,
+        };
+      });
+      this.localita_immobile = [
+        {
+          id: "all",
+          title: "Tutte",
+        },
+        ...this.localita_immobile,
+      ];
+    },
+    async setZoneOptions() {
+      await this.$store.dispatch("filters/fetchZonaImmobile");
+      this.zona_immobile = this.$store.state.filters.zona_immobile;
+      this.zona_immobile = this.zona_immobile.data.map((item) => {
+        return {
+          id: item.id,
+          title: item.attributes.title,
+        };
+      });
+      this.zona_immobile = [
+        {
+          id: "all",
+          title: "Tutte",
+        },
+        ...this.zona_immobile,
+      ];
+    },
+  },
+  created() {
+    this.setProvinceOptions();
+    this.setLocalitaOptions();
+    this.setZoneOptions();
   },
 };
 </script>
---
+++ components/filter/SelectComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="select" :tabindex="tabIndex || -1" @focusout="handleFocusOut">
+    <header class="select__header">
+      <h3 v-if="title" class="select__title filter-title">
+        <span class="select__span">{{ title }}</span>
+      </h3>
+      <div
+        :class="{
+          'is-active': isVisibleOptions,
+        }"
+        class="select-selected"
+        @click="toggleIsVisibleOptions"
+      >
+        <span v-if="svg.length > 0" v-html="svg"></span>
+        <span>{{ value }}</span>
+      </div>
+    </header>
+    <ul
+      class="select-options"
+      v-if="isVisibleOptions"
+      :class="{ overflow: options.length > 8 }"
+    >
+      <li
+        v-for="item in options"
+        :key="item[loopId]"
+        class="select-options__item"
+        @click="handleOptionClick({ id: item[id], label: item[label] })"
+      >
+        {{ item[label] }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    options: Array,
+    id: String,
+    title: String,
+    label: String,
+    value: String,
+    svg: String,
+    tabIndex: String,
+    loopId: String,
+  },
+  data: function () {
+    return {
+      isVisibleOptions: false,
+    };
+  },
+  methods: {
+    toggleIsVisibleOptions() {
+      this.isVisibleOptions = !this.isVisibleOptions;
+    },
+    handleOptionClick(value) {
+      this.$emit("handle-func", value);
+      this.isVisibleOptions = false;
+    },
+    handleFocusOut() {
+      this.isVisibleOptions = false;
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
